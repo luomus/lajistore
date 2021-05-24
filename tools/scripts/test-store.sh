aa168b81a8fc43e10359c9f5bec02d21a43a9642
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2021-05-24 14:57:45 +0300

    run tests with --build just in case the Dockerfile might have been changed

--- tools/scripts/test-store.sh
+++ tools/scripts/test-store.sh
@@ -7,7 +7,7 @@ cd ${SCRIPT_PATH}/../../
 docker volume rm luomus_elastic_data
 docker volume rm luomus_postgre_data
 
-docker-compose -f ./docker-compose.e2e.yml up test
+docker-compose -f ./docker-compose.e2e.yml up --build test
 #docker-compose -f ./docker-compose.e2e.yml up store-api
 docker-compose -f ./docker-compose.e2e.yml down --remove-orphans
 
