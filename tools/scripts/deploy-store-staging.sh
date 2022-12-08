aa51e239071f8f85fcc4715e1b61751ea149b886
Meeri Rannisto <aorin@users.noreply.github.com>
2022-12-08 09:50:38 +0200

    Update staging registry address

--- tools/scripts/deploy-store-staging.sh
+++ tools/scripts/deploy-store-staging.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPT_PATH}
 
 ./_deploy.sh \
-  "registry.apps.ocp-test-0.k8s.it.helsinki.fi/lajistore-api" \
+  "registry.ext.ocp-test-0.k8s.it.helsinki.fi/lajistore-api" \
   "https://api.ocp-test-0.k8s.it.helsinki.fi:6443" \
   "staging" \
   ${1}
