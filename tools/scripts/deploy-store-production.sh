606949edb638722fcdd36103d0c2039d20e55c55
Meeri Rannisto <aorin@users.noreply.github.com>
2023-01-09 12:31:09 +0200

    Update production registry address

--- tools/scripts/deploy-store-production.sh
+++ tools/scripts/deploy-store-production.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPT_PATH}
 
 ./_deploy.sh \
-  "registry.apps.ocp-prod-0.k8s.it.helsinki.fi/lajistore-api" \
+  "registry.ext.ocp-prod-0.k8s.it.helsinki.fi/lajistore-api" \
   "https://api.ocp-prod-0.k8s.it.helsinki.fi:6443" \
   "production" \
   ${1}
