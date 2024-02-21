8e875b3aa3f773bcbea54199002e4e7156d557e3
Markus Lumme <markusjuhani.lumme@gmail.com>
2024-02-21 11:56:02 +0200

    Revert "More config changes for automatic building."
    This reverts commit fb28b2d60c3920854025914738025058256efee9.

--- tools/scripts/generate-npm-shares.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-cd ${SCRIPT_PATH}/../../libs/shared/schemas
-
-npm run tsc
--- tools/scripts/publish-npm-shares.sh
+++ tools/scripts/publish-npm-shares.sh
@@ -5,6 +5,5 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ${SCRIPT_PATH}/../../libs/shared/schemas
 
-npm run tsc
 npm version patch
 npm publish
