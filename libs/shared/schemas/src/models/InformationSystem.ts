5b3d43724cd97bbbfd99b4e22f0f81637ee9f91d
Luomus CI <www@luomus.fi>
2024-05-28 00:42:32 +0000

    Updated shares

--- libs/shared/schemas/src/models/InformationSystem.ts
+++ libs/shared/schemas/src/models/InformationSystem.ts
@@ -28,6 +28,10 @@ export interface InformationSystem {
   "@type"?: string;
   URI: string;
   allowedDatawarehouseOverridingSourceId?: string[];
+  /**
+   * System is allowed to use data warehouse 'useEditorOrObserverIdIsNot' query param
+   */
+  allowedToUseDatawarehouseEditorOrObserverIdIsNot?: boolean;
   description: {
     en?: string;
     fi?: string;
