95a22e7073900442f1e79d3c402e8f92ec49022c
Markus Lumme <markusjuhani.lumme@gmail.com>
2024-06-05 15:40:24 +0300

    Revert "Changed export syntac in shared classes and interfaces to enable access to enums etc. in addition of the class or interface itself."
    This reverts commit eeccaa5341a2c74dafe8258caec4c0b98f488952.

--- libs/store/generate/src/lib/generate/generate-interface-or-class.service.ts
+++ libs/store/generate/src/lib/generate/generate-interface-or-class.service.ts
@@ -130,8 +130,6 @@ export function isLangObject<T>(value: unknown): value is Record<Lang, T> {
     '* and run the command to regenerate this file.\n' +
     '*/\n';
 
-    this.indexContent += `\nexport * from './${fileName}'`;
-
     if (this.type === 'interface') {
       this.indexContent += `\nimport { ${normalized} } from './${fileName}'`;
       this.indexContent += `\nexport type { ${normalized} }`;
@@ -144,6 +142,8 @@ export function is${normalized}(data: unknown): data is ${normalized} {
 }`
 
       this.models.push(normalized);
+    } else {
+      this.indexContent += `\nexport { ${normalized} } from './${fileName}'`;
     }
 
     let ownInterface = this.getOwnInterface(normalized, tsInterface);
